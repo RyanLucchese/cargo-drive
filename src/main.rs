aa977393c07b5e538a985dd2429752d98f8405d5
Ryan Lucchese <ryan@energi.team>
2022-06-13 07:53:40 -0600

    fixed argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::env;
 use std::process;
 use std::process::{Command, Stdio};
+use std::ffi::OsString;
 
 fn main() {
-    let mut args = env::args_os();
+    let mut args = env::args_os().peekable();
+
+    // we either have 1 extra arg for the executable name
+    // or 2 extra args if called as `cargo drive`
+    args.next();
+    if args.peek() == Some(&OsString::from("drive")) {
+        args.next();
+    }
 
     let status = Command::new("cargo")
         .stdin(Stdio::null())
         .stdout(Stdio::inherit())
         .arg("run")
-        .args(args.next())
+        .args(args)
         .status()
         .expect("failed to execute cargo");
 
